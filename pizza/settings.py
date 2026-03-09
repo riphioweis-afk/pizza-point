27c6dfab72437e12c13049bfec13fb8d00d34227
Riphi <riphioweis@gmail.com>
2026-03-09 06:14:00 -0400

    fix heroku deployment

--- pizza/settings.py
+++ pizza/settings.py
@@ -94,8 +94,10 @@ if "ON_HEROKU" in os.environ:
 else:
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pizza_db',
+        'USER': 'postgres',
+        'PASSWORD': '2324',
     }
 }
 
